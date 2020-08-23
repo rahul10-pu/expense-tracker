3fa865ee100585f8b19686c17ce7542eb3d139ef
Rahul Kumar <rahul10.pu@gmail.com>
2020-08-23 15:51:26 +0530

    added category controller with POST, PUT, DELETE request

--- src/main/java/com/example/upgrad/sample/controller/CategoryController.java
+++ src/main/java/com/example/upgrad/sample/controller/CategoryController.java
@@ -1,14 +1,23 @@
 package com.example.upgrad.sample.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.upgrad.sample.model.Category;
 import com.example.upgrad.sample.repository.CategoryRepository;
 
@@ -32,5 +41,26 @@ public class CategoryController {
 	return category.map(response -> ResponseEntity.ok().body(response))
 			.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 			 
+	}
+	@PostMapping("/category")
+	ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException{
+	  Category result= categoryRepository.save(category);
+	  return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result); 
+	
+	}
+	
+	
+	@PutMapping("/category/{id}")
+	ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category){
+		Category result= categoryRepository.save(category);
+		return ResponseEntity.ok().body(result);
+	}
+	
+	
+	
+	@DeleteMapping("/category/{id}")
+	ResponseEntity<?> deleteCategory(@PathVariable Long id){
+		categoryRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
