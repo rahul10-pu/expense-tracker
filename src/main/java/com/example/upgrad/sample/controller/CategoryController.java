82f0333fb718e89881e933da03c2a3d51174dd99
Rahul Kumar <rahul10.pu@gmail.com>
2020-08-23 15:45:57 +0530

    added category/{id} controller

--- src/main/java/com/example/upgrad/sample/controller/CategoryController.java
+++ src/main/java/com/example/upgrad/sample/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package com.example.upgrad.sample.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.upgrad.sample.model.Category;
@@ -20,5 +24,13 @@ public class CategoryController {
 	@GetMapping("/categories")
 	Collection<Category> categories(){
 		return categoryRepository.findAll();
+	}
+	//category/2
+	@GetMapping("/category/{id}")
+	ResponseEntity<?> getCategory(@PathVariable Long id){
+	Optional<Category> category = categoryRepository.findById(id);
+	return category.map(response -> ResponseEntity.ok().body(response))
+			.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+			 
 	}
 }
